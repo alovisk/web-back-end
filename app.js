014cad6db253a20d7f732c70d52ab289fd73c99e
alovisk <allyson.cunha.cunha@hotmail.com>
2017-11-26 15:49:37 -0200

    teste

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.get('/', function(req, res){
   app.get('/img', function(req, res){
     res.send("http://www.craceara.org.br/images/FANOR.png");
   });
-  app.get('/r', function(req, res){
-    var teste = req.param('teste');
-    res.send(teste);
+  app.get('/getSql', function(req, res){
+    var sql = req.param('sql');
+    console.log(teste);
   });
 
   var server = app.listen(3000);
